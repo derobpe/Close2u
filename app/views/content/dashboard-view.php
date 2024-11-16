48bb569a939fc99498d5fec4bb78889cb4603bbf
Debora <derobpe@gmail.com>
2024-11-16 19:12:59 +0100

    dashboard and userImage

--- app/views/content/dashboard-view.php
+++ app/views/content/dashboard-view.php
@@ -1,1 +1,19 @@
-Home
+<div class="container is-fluid">
+    <h1 class="title has-text-centered">Home</h1>
+
+    <!-- Image -->
+    <div class="columns is-centered">
+        <div class="column is-narrow">
+            <figure class="image is-128x128">
+                <img class="is-rounded" src="<?php echo APP_URL; ?>app/views/photos/user_1.png" alt="User Image">
+            </figure>
+        </div>
+    </div>
+
+    <!-- Title -->
+    <div class="columns is-centered">
+        <div class="column is-narrow">
+            <h2 class="subtitle has-text-centered">Welcome username</h2>
+        </div>
+    </div>
+</div>
