0668dfa015c6df6f50604a03df583486bb668dd1
Debora <derobpe@gmail.com>
2024-11-16 17:03:09 +0100

    fix: Enhance autoloader with error logging and improved exception handling

--- autoload.php
+++ autoload.php
@@ -1,10 +1,10 @@
 <?php
 spl_autoload_register(function ($class) {
-
     $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-    if(is_file($file)) {
+    if (is_file($file)) {
         require_once $file;
+    } else {
+        error_log("Autoload: The class '$class' could not be loaded. File '$file' not found.");
+        throw new Exception("The class '$class' could not be loaded.");
     }
-    throw new Exception("The class '$class' could not be loaded.");
-
 });
