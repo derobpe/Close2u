053215e2196ee6ba522a898b424808ce66013bc6
Debora <derobpe@gmail.com>
2024-11-16 15:51:15 +0100

    fix: Update APP_URL to include port 8080 for Bulma styles

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
-    const APP_URL = "http://localhost/Close2u/";
-    const APP_NAME = "Close2u POO MySQL";
+    const APP_URL = "http://localhost:8080/Close2u/";
+    const APP_NAME = "Close2u";
     const APP_SESSION_NAME = "Close2u";
 
     date_default_timezone_set("Europe/Madrid");
