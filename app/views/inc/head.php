647b0221661ac3d75e9ff8200aaacfd0e601c238
Debora <derobpe@gmail.com>
2024-11-16 16:16:26 +0100

    chore: Install SweetAlert2 plugin for enhanced alerts

--- app/views/inc/head.php
+++ app/views/inc/head.php
@@ -3,3 +3,5 @@
 <title><?php echo APP_NAME; ?></title>
 <link rel="stylesheet" href="<?php echo APP_URL;?>app/views/css/bulma.min.css">
 <link rel="stylesheet" href="<?php echo APP_URL;?>app/views/css/styles.css">
+<link rel="stylesheet" href="<?php echo APP_URL;?>app/views/css/sweet_alert2_min.css">
+<script src="<?php echo APP_URL; ?>app/views/js/sweet_alert2_min.js"></script>
